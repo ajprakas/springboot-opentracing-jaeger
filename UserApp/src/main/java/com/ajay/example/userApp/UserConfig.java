3e5d24668b08cf62d2745e21b66d911df64d8195
Ajay Prakash <ajprakash@vmware.com>
2021-03-12 19:53:32 +0530

    springboot App with jaeger tracing

--- UserApp/src/main/java/com/ajay/example/userApp/UserConfig.java
+++ UserApp/src/main/java/com/ajay/example/userApp/UserConfig.java
@@ -6,18 +6,22 @@ import io.jaegertracing.Configuration.SamplerConfiguration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
 import io.jaegertracing.internal.samplers.ProbabilisticSampler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan("com.ajay.example.userApp")
 public class UserConfig {
-
+    @Value("${jaeger.agent.host}")
+    private String jaegerAgentHost;
+    @Value("${jaeger.agent.port}")
+    private String jaegerAgentPort;
     @Bean
     public JaegerTracer jaegerTracer(){
         Configuration config = new Configuration("user-service")
                                                 .withSampler(new SamplerConfiguration().withType(ConstSampler.TYPE).withParam(1))
-                                                .withReporter(new ReporterConfiguration().withLogSpans(true));
+                                                .withReporter(new ReporterConfiguration().withSender(new Configuration.SenderConfiguration().withAgentHost(jaegerAgentHost).withAgentPort(Integer.valueOf(jaegerAgentPort))).withLogSpans(true));
         return config.getTracer();
     }
 
--- mongo-service/src/main/java/com/ajay/example/mongo/UserDBConfig.java
+++ mongo-service/src/main/java/com/ajay/example/mongo/UserDBConfig.java
@@ -3,15 +3,20 @@ package com.ajay.example.mongo;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class UserDBConfig {
+    @Value("${jaeger.agent.host}")
+    private String jaegerAgentHost;
+    @Value("${jaeger.agent.port}")
+    private String jaegerAgentPort;
     @Bean
     public JaegerTracer jaegerTracer(){
         Configuration config = new Configuration("mongo-service")
                 .withSampler(new Configuration.SamplerConfiguration().withType(ConstSampler.TYPE).withParam(1))
-                .withReporter(new Configuration.ReporterConfiguration().withLogSpans(true));
+                .withReporter(new Configuration.ReporterConfiguration().withSender(new Configuration.SenderConfiguration().withAgentHost(jaegerAgentHost).withAgentPort(Integer.valueOf(jaegerAgentPort))).withLogSpans(true));
         return config.getTracer();
     }
 }
